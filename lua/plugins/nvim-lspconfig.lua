94da5e9d925954d02e624b611b09dad8b9ed0401
mdan16 <t.t.mina16@gmail.com>
2024-04-03 18:11:13 +0900

    update

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -6,23 +6,29 @@ return {
         { "antosha417/nvim-lsp-file-operations", config = true },
     },
     config = function()
-        -- local lspconfig = require("lspconfig")
-        -- local cmp_nvim_lsp = require("cmp_nvim_lsp")
-        -- local capabilities = cmp_nvim_lsp.default_capabilities()
-        -- local opts = { noremap = true, silent = true }
-        -- local on_attach = function(_, bufnr)
-        --     opts.buffer = bufnr
+        local lspconfig = require("lspconfig")
+        local cmp_nvim_lsp = require("cmp_nvim_lsp")
+        local capabilities = cmp_nvim_lsp.default_capabilities()
+        local opts = { noremap = true, silent = true }
+        local on_attach = function(_, bufnr)
+            opts.buffer = bufnr
 
-        --     opts.desc = "Show line diagnostics"
-        --     vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+            vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+            vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+            vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
 
-        --     opts.desc = "Show documentation for what is under cursor"
-        --     vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-        -- end
+            opts.desc = "Show line diagnostics"
+            vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
 
-        -- lspconfig["sourcekit"].setup({
-        --     capabilities = capabilities,
-        --     on_attach = on_attach,
-        -- })
+            opts.desc = "Show documentation for what is under cursor"
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        end
+
+        lspconfig["sourcekit"].setup({
+            capabilities = capabilities,
+            on_attach = on_attach,
+        })
     end,
 }
